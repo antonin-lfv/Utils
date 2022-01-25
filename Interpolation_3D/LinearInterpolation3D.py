72e1ccff5c6853cf8b2b6bcdcff01cabe422b402
antonin-lfv <antoninlefevre45@icloud.com>
2022-01-25 13:43:08 +0100

    init interpolation class

--- Interpolation_3D/LinearInterpolation3D.py
+++ Interpolation_3D/LinearInterpolation3D.py
@@ -1,14 +1,14 @@
 """ 3D plot with matplotlib """
-
-# import
+import numpy
 from matplotlib.ticker import MaxNLocator
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
+mpl.use('macosx')
 
-class Interpolator:
+class Interpolator():
     def __init__(self, matrix):
         self.matrix = matrix
         self.row_mat, self.col_mat = self.matrix.shape
@@ -19,14 +19,14 @@ class Interpolator:
     def __repr__(self):
         return f"Interpolator({self.matrix})"
 
-    def zprime(self):
+    def __zprime(self):
         l = []
         for j in reversed(range(self.col_mat)):
             for i in reversed(range(self.row_mat)):
-                l.append((Z[i])[j])
+                l.append((self.matrix[i])[j])
         return l
 
-    def data_array(self):
+    def __data_array(self):
         c = []
         for i in range(self.row_mat):
             for j in range(self.col_mat):
@@ -41,7 +41,7 @@ class Interpolator:
         """ With lines """
         # create the 3D plot
         plt.rcParams['legend.fontsize'] = 10
-        fig = plt.figure()
+        fig = plt.figure('Line plot')
         ax = fig.add_subplot(projection='3d')
         # data
         xp = [i for i in reversed(range(0, self.col_mat))]
@@ -52,29 +52,26 @@ class Interpolator:
             zi = self.matrix[i]
             for j in reversed(range(0, self.col_mat)):
                 xl = [j] * self.row_mat
-                ziprime = self.zprime()[(j * self.row_mat):(j * self.row_mat + self.row_mat)]
-                plt.plot(xp, yp, zi, 'blue')
-                plt.plot(xl, yl, ziprime, 'red')
+                ziprime = self.__zprime()[(j * self.row_mat):(j * self.row_mat + self.row_mat)]
+                plt.plot(xp, yp, zi, 'blue', linewidth=1)
+                plt.plot(xl, yl, ziprime, 'red', linewidth=1)
         plt.xlabel("x")
         plt.ylabel("y")
         plt.xlim(0, self.col_mat)
         plt.ylim(0, self.row_mat)
-        plt.title("linear interpolation")
-        plt.show()
+        return fig
 
     def graph_3D_color(self):
         """ With Gradient color """
-        fig = plt.figure()
+        fig = plt.figure('Gradient color')
         ax = fig.add_subplot(projection='3d')
-        Xs = self.data_array()[:, 0]
-        Ys = self.data_array()[:, 1]
-        Zs = self.data_array()[:, 2]
-        surf = ax.plot_trisurf(Xs, Ys, Zs, cmap=cm.jet, linewidths=1)
-        # cmap='viridis', edgecolor='none')
+        Xs = self.__data_array()[:, 0]
+        Ys = self.__data_array()[:, 1]
+        Zs = self.__data_array()[:, 2]
+        surf = ax.plot_trisurf(Xs, Ys, Zs, cmap=cm.ocean, linewidths=1, edgecolor='none')
         fig.colorbar(surf)
         ax.xaxis.set_major_locator(MaxNLocator(5))
         ax.yaxis.set_major_locator(MaxNLocator(6))
         ax.zaxis.set_major_locator(MaxNLocator(5))
         fig.tight_layout()
-        ax.set_title('linear interpolation')
-        plt.show()
+        return fig
--- Interpolation_3D/example.py
+++ Interpolation_3D/example.py
@@ -1,6 +1,9 @@
 """ Usage example """
+import matplotlib.pyplot as plt
 
 from Interpolation_3D.LinearInterpolation3D import *
+import pandas as pd
+z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
 
 Z = np.array([[7.894736842105264, 7.894736842105264, 7.894736842105264, 8.947368421052632, 8.947368421052632, 10.0,
                10.0, 10.0, 10.0, 10.0, 10.0],
@@ -23,10 +26,11 @@ Z = np.array([[7.894736842105264, 7.894736842105264, 7.894736842105264, 8.947368
               [-0.5263157894736843, 0.5263157894736843, 1.5789473684210527, 2.6315789473684212, 3.6842105263157894,
                3.6842105263157894, 5.789473684210527, 5.789473684210527, 6.842105263157896, 7.894736842105264, 7.9]])
 
-mon_interpolateur = Interpolator(matrix=Z)
-
-# With color gradient
-mon_interpolateur.graph_3D_color()
-
-# With lines
-mon_interpolateur.graph_3D_line()
+if __name__ == "__main__":
+    mon_interpolateur = Interpolator(matrix=z_data.values)
+    # With color gradient
+    f1 = mon_interpolateur.graph_3D_color()
+    # With lines
+    f2 = mon_interpolateur.graph_3D_line()
+    # Plot the 2 fig
+    plt.show()
