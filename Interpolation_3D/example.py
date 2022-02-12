1aa2b919762c7d912119cde31fb2af8e8a9e09b9
antonin-lfv <antoninlefevre45@icloud.com>
2022-02-12 17:32:22 +0100

    init interpolation class

--- Interpolation_3D/example.py
+++ Interpolation_3D/example.py
@@ -5,13 +5,14 @@ import numpy as np
 
 m1 = np.fromfunction(lambda x, y: np.sin(x/5)+np.cos(y/5)+0.1, (20, 20))
 m2 = np.fromfunction(lambda x, y: np.cos(np.sin(x)+2*y+1)-5, (30, 30))
+m3 = np.fromfunction(lambda x, y: np.sqrt(x**2+y**2), (15, 30))
 
 z_data_1 = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
 z_data_2 = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/3d_line_sample_data.csv', nrows=110, skiprows=10)
 z_data_3 = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/line_3d_dataset.csv')
 
 if __name__ == "__main__":
-    mon_interpolateur = Interpolator(matrix=m2)
+    mon_interpolateur = Interpolator(matrix=m3)
     # With color gradient (unique plot)
     f1 = mon_interpolateur.graph_3D_color(display=False)  # display=True to juste plot this figure
 
