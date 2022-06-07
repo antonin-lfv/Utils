6b12998225dcdef5d2687eed4892610aeb428b17
Antonin <antoninlefevre45@icloud.com>
2022-06-07 22:38:03 +0200

    Update example.py

--- PlotGraph/D3Graph/example.py
+++ PlotGraph/D3Graph/example.py
@@ -4,5 +4,5 @@ from PlotGraph.D3Graph.D3GraphPlot import *
 node_csv_3D = pd.read_csv('data/neouds_3D.csv', sep=';')
 edge_csv_3D = pd.read_csv('data/aretes_3D.csv', sep=';')
 # Plot
-fig_3D = graphe_3d(nodes=node_csv_3D, edges=edge_csv_3D)
+fig_3D = graphe_3d(nodes=node_csv_3D, edges=edge_csv_3D, showEdges=True)
 plot(fig_3D)
