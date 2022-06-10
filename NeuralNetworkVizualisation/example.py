0d9759a2bd37f120fe43db7e3150ddd6cc735ded
Antonin <antoninlefevre45@icloud.com>
2022-06-10 22:29:12 +0200

    fix

--- NeuralNetworkVizualisation/example.py
+++ NeuralNetworkVizualisation/example.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     model.add(Dense(13, activation='softmax'))
     model.add(Dense(10, activation='sigmoid'))
     model.add(Dense(4, activation='softmax'))
-    model.add(Dense(4, activation='relu'))
+    model.add(Dense(14, activation='relu'))
     model.add(Dense(1))
 
     MyFirstNN = VizNN(model)
