8fc4c270ca5df067acdebb4a3879858d414bcbcf
Antonin <antoninlefevre45@icloud.com>
2022-09-29 21:15:31 +0200

    streamlit generation 3D index on graphe

--- PlotGraph/D3Graph/D3GraphPlot.py
+++ PlotGraph/D3Graph/D3GraphPlot.py
@@ -66,8 +66,9 @@ def graphe_3d(nodes, edges, showEdges=True):
 
     node_trace = go.Scatter3d(
         x=node_x, y=node_y, z=node_z,
+        text=nodes['n'].to_list(),
         mode='markers',
-        hovertemplate='(%{x};%{y};%{z})',
+        hovertemplate='(%{x};%{y};%{z})\nIndex: %{text}',
         marker=dict(
             color='black',
             size=5),
